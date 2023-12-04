d857a33ea063a1e99ef3c9bf6f2a2359c6d76531
Pedro <pedro.dpaula67@gmail.com>
2023-12-04 13:35:16 -0300

    Update 001.js

--- desafios/001.js
+++ desafios/001.js
@@ -13,6 +13,6 @@ Você terá 3 variáveis. Sendo elas:
  const distanciaEmKm = 100;
 
  const litrosConsumidos = distanciaEmKm / kmPorLitros;
- const gastoViagem = litrosConsumidos * precoCombustivel;
+ const valorGasto = litrosConsumidos * precoCombustivel;
 
-console.log(gastoViagem);
+console.log(valorGasto.toFixed(2));
