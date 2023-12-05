4f3e0df45b181a436d35f8a7affc9ef093f69585
Pedro <pedro.dpaula67@gmail.com>
2023-12-04 23:41:35 -0300

    Create desafio.js

---
+++ 02-condicionais/desafio.js
@@ -0,0 +1,25 @@
+/* Faça um programa para calcular o valor de uma viagem.
+
+Você terá 5 variáveis. Sendo elas:
+ 1 - Preço do etanol;
+ 2 - preço da gasolina;
+ 3 - O tipode combustível que está no seu carro;
+ 4 - Gasto médio de combustível do carro por KM;
+ 5 - Distância em KM da viagem;
+ 
+ Impima no console o valor que será gasto para realizar a viagem.*/
+
+const precoEtanol = 4;
+const precoGasolina = 5;
+const mediaCombustivelPorKm = 1;
+const distanciaViagemEmKm = 50;
+
+const combustivelTipo = precoEtanol;
+
+const litrosConsumidos = distanciaViagemEmKm / mediaCombustivelPorKm;
+
+if(combustivelTipo = precoEtanol){
+    litrosConsumidos * precoEtanol
+} else(combustivelTipo = precoGasolina) {
+    litrosConsumidos * precoGasolina
+}
