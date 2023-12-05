26c5fbb77ccc409358a22d551b5f8ddb4584643d
Pedro <pedro.dpaula67@gmail.com>
2023-12-04 23:24:29 -0300

    Create aula.js

---
+++ 02-condicionais/aula.js
@@ -0,0 +1,13 @@
+
+
+const numero = 0;
+
+const numeroDivisivelPor5 = (numero % 5) === 0; 
+
+if(numero === 0){
+    console.log('o número é inválido');
+} else if(numeroDivisivelPor5){
+    console.log('sim')
+} else {
+    console.log('não');
+}
