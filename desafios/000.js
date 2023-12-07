da47d04b54c47b78f1e82c22ec1e8b22a5bf2799
Pedro <pedro.dpaula67@gmail.com>
2023-12-07 14:58:33 -0300

    dessfios/exercicios

---
+++ desafios/000.js
@@ -0,0 +1,10 @@
+const valor1 = 134;
+const valor2 = 100;
+const valor3 = 20;
+
+
+const resultado = (valor2 * valor3) / valor1;
+/* console.log(''/valor1/' ---- '/valor2/'');
+console.log(''/valor3/' ---- X '); */
+
+console.log(resultado.toFixed(2));
